33b957bbd92d7e84c12fc1cfc8965c15292b2aea
SilverPeak2299 <dannydickerson03@gmail.com>
2024-05-09 17:20:48 +1000

    First Commit

--- DataCollection/fencerInfo.py
+++ DataCollection/fencerInfo.py
@@ -1,11 +1,12 @@
 class FencerData:
     def __init__(self, licenceNo):
         # Data Scraped From fencingdatabase.com
-        self.licenceNo = licenceNo
+        self.ID = int(licenceNo)
         self.name = ""
         self.country = ""
+        self.totalTouches = 0
         self.favouriteTarget = ""
-        self.opponents = []
+        self.opponents = {} # fencer id will be the key with point delta as the data
 
         # Data Scraped from fie.org
         self.BestYear = 0
@@ -15,6 +16,22 @@ class FencerData:
         self.Handedness = ""
         self.Age = 0
 
-    
+class BoutInfo:
+    def __init__(self, fencer1, fencer2, f1Touches, f2Touches):
+        # ensuring the smallest number always goes first
+        if fencer1 < fencer2:
+            self.fencer1 = fencer1
+            self.fencer2 = fencer2
+        else:
+            self.fencer1 = fencer2
+            self.fencer2 = fencer1
+
+        self.f1Touches = f1Touches
+        self.f2Touches = f2Touches
+        self.delta = f1Touches - f2Touches # f1 - f2
+
+        self.winner = self.fencer1 if self.delta < 0 else self.fencer2
+
+
 if __name__ == "__main__":
     pass
--- DataCollection/webscraper.py
+++ DataCollection/webscraper.py
@@ -7,7 +7,7 @@ class webscraper:
     def __init__(self):
         self.fencers = []
 
-    def fencingDBSearch(self):
+    def generateFencerList(self):
         #List of all male sabre fencers in the database
         url = "https://fencingdatabase.com/?firstname=&lastname=&weapon=sabre&gender=male&tournament=all&year=all&opposing-score=0&opposing-lastname=&submit-search=Search+Fencers"
         page = requests.get(url)
@@ -23,10 +23,27 @@ class webscraper:
                 continue
 
             self.fencers.append(fd(linkStr.removeprefix("/fencers/")))
-            
+
+    def collectFencerData(self):
+        baseUrl = "https://www.fencingdatabase.com/fencers/"
+
+        for fencer in self.fencers:
+            fencerPage = requests.get(baseUrl + fencer.ID)
+            soup = bs(fencerPage.content, "html.parser")
+
+            fencer.name = soup.find("div", class_="fencer-name").removeprefix("Name: ")
+            fencer.country = soup.find("div", class_="fencer-country").removeprefix("Country Code: ")
+            fencer.totalTouches = int(soup.find("div", class_="fencer-touches").removeprefix("Total touches: "))
+            fencer.favouriteTarget = soup.find("div", class_="fencer-hit-location").removeprefix("Favourite Place To Score: ")
+
+            # finding opponents and hit delta
+
+            for 
 
 
+        
 
 if __name__ == "__main__":
     test = webscraper()
-    test.fencingDBSearch()
+    test.generateFencerList()
+    test.collectFencerData()
